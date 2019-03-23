aa2526153f950d0b18d7a6eaf8bd9958f6dd650e
saifrais <saifrais@gmail.com>
2019-03-23 08:53:31 +0000

    Vision API parsed over 115,000 frames

---
+++ code/gcs_utils.py
@@ -0,0 +1,107 @@
+#https://github.com/GoogleCloudPlatform/python-docs-samples/blob/master/storage/cloud-client/snippets.py
+# Accessing the Google Cloud Storage
+from google.cloud import storage
+
+def list_blobs(bucket_name):
+    """Lists all the blobs in the bucket."""
+    storage_client = storage.Client()
+    bucket = storage_client.get_bucket(bucket_name)
+
+    blobs = bucket.list_blobs()
+
+    for blob in blobs:
+        print(blob.name)
+        
+def list_blobs_with_prefix(bucket_name, prefix, delimiter=None):
+    """Lists all the blobs in the bucket that begin with the prefix.
+
+    This can be used to list all blobs in a "folder", e.g. "public/".
+
+    The delimiter argument can be used to restrict the results to only the
+    "files" in the given "folder". Without the delimiter, the entire tree under
+    the prefix is returned. For example, given these blobs:
+
+        /a/1.txt
+        /a/b/2.txt
+
+    If you just specify prefix = '/a', you'll get back:
+
+        /a/1.txt
+        /a/b/2.txt
+
+    However, if you specify prefix='/a' and delimiter='/', you'll get back:
+
+        /a/1.txt
+
+    """
+    storage_client = storage.Client()
+    bucket = storage_client.get_bucket(bucket_name)
+
+    blobs = bucket.list_blobs(prefix=prefix, delimiter=delimiter)
+    
+    #print('Blobs:')
+    #for blob in blobs:
+    #    print(blob.name)
+
+    #if delimiter:
+    #    print('Prefixes:')
+    #    for prefix in blobs.prefixes:
+    #        print(prefix)
+    
+    all_img_uri = []
+    for img_uri in blobs:
+        all_img_uri.append(img_uri.name)
+        len(all_img_uri)
+            
+    return(all_img_uri)
+
+# [START storage_upload_file]
+def upload_blob(bucket_name, source_file_name, destination_blob_name):
+    """Uploads a file to the bucket."""
+    storage_client = storage.Client()
+    bucket = storage_client.get_bucket(bucket_name)
+    blob = bucket.blob(destination_blob_name)
+
+    blob.upload_from_filename(source_file_name)
+
+    print('File {} uploaded to {}.'.format(
+        source_file_name,
+        destination_blob_name))
+# [END storage_upload_file]
+
+def download_blob(bucket_name, source_blob_name, destination_file_name):
+    """Downloads a blob from the bucket."""
+    storage_client = storage.Client()
+    bucket = storage_client.get_bucket(bucket_name)
+    blob = bucket.blob(source_blob_name)
+
+    blob.download_to_filename(destination_file_name)
+
+    print('Blob {} downloaded to {}.'.format(
+        source_blob_name,
+        destination_file_name))
+
+
+def delete_blob(bucket_name, blob_name):
+    """Deletes a blob from the bucket."""
+    storage_client = storage.Client()
+    bucket = storage_client.get_bucket(bucket_name)
+    blob = bucket.blob(blob_name)
+
+    blob.delete()
+
+    print('Blob {} deleted.'.format(blob_name))
+    
+    
+def save_obj(obj, storeloc = "dict.pickle"):
+    pickle_out = open(storeloc,"wb")
+    pickle.dump(obj, pickle_out)
+    print('{} saved'.format(storeloc))
+    pickle_out.close()
+    
+def load_obj(storeloc="accident_df.pickle"):
+    import pandas as pd
+    pickle_in = open(storeloc,"rb")
+    obj = pickle.load(pickle_in)
+    print('{} loaded'.format(storeloc))
+    return obj
